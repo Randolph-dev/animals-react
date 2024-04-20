8953bade54431402931652358ab4cac78a38436a
Randolph <86254950+RR2894@users.noreply.github.com>
2024-04-20 17:07:28 +0300

    added rows and added variable to capitalize animal names

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
 import './index.css'
 
 const Card = ({ name }) => {
+    const capitalizedLetters = name.charAt(0).toUpperCase() + name.slice(1);
     return (
         <div className='card'>
-            <h2>{name}</h2>
-            <img src={`https://source.unsplash.com/random/400x400/?${name}`} alt={name} />
+            <h2>{capitalizedLetters}</h2>
+            <img src={`https://source.unsplash.com/random/200x200/?${name}`} alt={capitalizedLetters} />
         </div>
     )
 }
