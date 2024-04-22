352f99398b31e2c6c4ef9f74dd0bbdd02926d126
Randolph <86254950+RR2894@users.noreply.github.com>
2024-04-22 14:49:38 +0300

    Added likes/dislike function and css styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,38 @@ import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Card from './Card.jsx';
 
-function Cards() {
-  const [cards, setCards] = useState('');
-  const [cardData, setCardData] = useState({
-    animals: ' ',
-    birds: ' ',
-    fishes: ' ',
-    insects: ' ',
-  })
-}
-
-
 function App() {
+
+  const [animalsData, setAnimalsData] = useState(animals);
+
+  const removeCard = (animalName) => {
+    const updatedArray = animalsData.filter(animal => animal.name !== animalName);
+    setAnimalsData(updatedArray);
+  };
+
+  const removeLikes = () => {
+    console.log('removed likes');
+  };
+  const addLikes = () => {
+    console.log('added likes');
+  };
+
   return (
     <>
     <Header />
     <main>
-        {animals.map((animal) => (
-          <Card key={animal.name} {...animal} />
+        {animalsData.map((animal) => (
+          <Card key={animal.name} 
+          {...animal} 
+          removeLikes={() => removeLikes(animal.name, 'remove')} 
+          addLikes={ addLikes.bind (this, animal.name, 'add')} 
+          removeCard={() => removeCard(animal.name)} />
+          ))}
 
-        ))}
     </main>
     <Footer />
     </>
   );
 
 }
-
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,35 @@
-import './index.css'
+import React, { useState } from 'react';
+import './index.css';
 
-const Card = ({ name }) => {
+const Card = ({ name, addLikes, removeLikes, removeImage }) => {
     const capitalizedLetters = name.charAt(0).toUpperCase() + name.slice(1);
+    const [likeCounter, setLikeCounter] = useState(0);
+
+    const handleAddLikes = () => {
+        setLikeCounter(likeCounter + 1);
+        addLikes();
+    };
+
+    const handleRemoveLikes = () => {
+        setLikeCounter(likeCounter - 1);
+        removeLikes();
+    };
+
+    const handleRemoveImage = () => {
+        removeImage(name);
+    };
+
     return (
         <div className='card'>
             <h2>{capitalizedLetters}</h2>
             <img src={`https://source.unsplash.com/random/200x200/?${name}`} alt={capitalizedLetters} />
+            <br />
+            <button className='likes' alt="likes" onClick={handleAddLikes}>Add Likes</button>
+            <button className='dislikes' alt="dislikes" onClick={handleRemoveLikes}>Remove Likes</button>
+            <button className='removeImage' alt="removeImage" onClick={handleRemoveImage}>Remove Image</button>
+            <p>Number of likes: {likeCounter} </p>
         </div>
-    )
-}
+    );
+};
 
 export default Card;
